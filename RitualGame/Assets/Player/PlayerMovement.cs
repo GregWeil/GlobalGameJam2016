2fe94f2ddab47d1215dcba4074f014594483cc89
Greg Weil <gregweil@pentavera.com>
2016-01-30 04:36:08 -0500

    Hold the jump button to jump slightly higher. It isn't enough to scale two block walls, but you can clear longer gaps

--- RitualGame/Assets/Player/PlayerMovement.cs
+++ RitualGame/Assets/Player/PlayerMovement.cs
@@ -9,9 +9,11 @@ public class PlayerMovement : MonoBehaviour {
 	public float moveAccel = 1.0f;
 
 	public float jumpForce = 1.0f;
+    public float jumpCarry = 1.0f;
 
 	public Vector2 gravity = Vector2.zero;
 
+    bool grounded = false;
 	bool jump = false;
 
 	CircleCollider2D col = null;
@@ -28,17 +30,21 @@ public class PlayerMovement : MonoBehaviour {
 		float goalSpeed = (Input.GetAxis("Horizontal"+playerNum.ToString()) * moveSpeed);
 		float force = (goalSpeed - body.velocity.x) * moveAccel;
 		body.AddForce(new Vector2(force, 0));
-		if (jump) body.AddForce (new Vector2 (0, jumpForce), ForceMode2D.Impulse);
-		jump = false;
+
+        if (jump) {
+            body.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+        } else if (Input.GetButton("Jump" + playerNum.ToString()) && !grounded) {
+            body.AddForce(new Vector2(0, jumpCarry));
+        }
+        jump = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetButtonDown ("Jump" + playerNum.ToString ())) {
-			RaycastHit2D hit = Physics2D.CircleCast (body.position, 0.9f*col.radius, Vector2.down, 0.1f, LayerMask.GetMask("Solid"));
-			if (hit.collider != null) {
-				jump = true;
-			}
+		RaycastHit2D hit = Physics2D.CircleCast (body.position, 0.9f*col.radius, Vector2.down, 0.1f, LayerMask.GetMask("Solid"));
+        grounded = ((hit.collider != null) && !jump);
+		if (Input.GetButtonDown ("Jump" + playerNum.ToString ()) && grounded) {
+			jump = true;
 		}
 	}
 }
