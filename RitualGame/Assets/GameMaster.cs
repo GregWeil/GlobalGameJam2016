4e74683b82cb8019c95387117d14109fe4e7e14f
samsaksfithian <saksfs@rpi.edu>
2016-01-30 23:09:53 -0500

    UI and setup features

--- RitualGame/Assets/GameMaster.cs
+++ RitualGame/Assets/GameMaster.cs
@@ -46,6 +46,11 @@ public class GameMaster : MonoBehaviour {
 	PlayerMovement rightPlayer = null;
 	GodHandAnimation godPlayer = null;
 
+	//TODO: Main/start menu
+	//TODO: Endgame screen
+	//TODO: Pause between rounds
+	//TODO: UI on totems/pedestal (once sprites are in)
+
 	void Start(){
 		GameObject[] totems = GameObject.FindGameObjectsWithTag ("Totem");
 		Debug.Assert (totems.Length == 2);
@@ -97,6 +102,7 @@ public class GameMaster : MonoBehaviour {
 		godPlayer.playerNum = rounds[roundNumber,1];
 		rightSpawn.playerNumber = rightPlayer.playerNum = rounds[roundNumber,2];
 		//set sprites
+		//TODO: color swap the animations
 		leftPlayer.GetComponentInChildren<SpriteRenderer>().sprite = playerSprites[rounds[roundNumber,0]];
 		leftTotem.sprite = totemSprites[rounds[roundNumber,0]];
 		godPlayer.handOpen = godOpenSprites[rounds[roundNumber,1]];
