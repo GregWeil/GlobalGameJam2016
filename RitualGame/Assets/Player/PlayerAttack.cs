54fc5b7edbfd19482a92977a442ca17e0cf5292b
Greg Weil <gregweil@pentavera.com>
2016-01-30 23:23:27 -0500

    Complete animations for the red character. Also moved the trees closer and tweaked player movement

--- RitualGame/Assets/Player/PlayerAttack.cs
+++ RitualGame/Assets/Player/PlayerAttack.cs
@@ -9,28 +9,40 @@ public class PlayerAttack : MonoBehaviour {
 
 	Collider2D col = null;
 	Rigidbody2D body = null;
+	Animator anim = null;
 	int controlNum;
 
 	// Use this for initialization
 	void Start () {
 		col = GetComponent<Collider2D> ();
 		body = GetComponent<Rigidbody2D> ();
+		anim = GetComponentInChildren<Animator> ();
 		controlNum = GetComponent<PlayerMovement> ().controlNum;
+	}
+
+	IEnumerator Attack () {
+		anim.SetTrigger ("Attack");
+		yield return new WaitForSeconds (0.1f);
+		Vector2 dir = GetComponentInChildren<SpriteRenderer> ().flipX ? Vector2.left : Vector2.right;
+		var hits = Physics2D.CircleCastAll (body.position, attackRadius, dir, attackRange, LayerMask.GetMask ("Player"));
+		foreach (var hit in hits) {
+			if (hit.collider != col) {
+				hit.collider.SendMessage ("Stun", 5.0f, SendMessageOptions.DontRequireReceiver);
+				hit.collider.SendMessage ("DropIdol", SendMessageOptions.DontRequireReceiver);
+				Vector2 push = (dir + new Vector2 (0, 0.3f)) * knockback;
+				hit.collider.GetComponent<Rigidbody2D> ().AddForce (push, ForceMode2D.Impulse);
+			}
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetButtonDown ("Attack" + controlNum.ToString ())) {
-			Vector2 dir = GetComponentInChildren<SpriteRenderer> ().flipX ? Vector2.left : Vector2.right;
-			var hits = Physics2D.CircleCastAll (body.position, attackRadius, dir, attackRange, LayerMask.GetMask ("Player"));
-			foreach (var hit in hits) {
-				if (hit.collider != col) {
-					hit.collider.SendMessage ("Stun", 5.0f, SendMessageOptions.DontRequireReceiver);
-					hit.collider.SendMessage ("DropIdol", SendMessageOptions.DontRequireReceiver);
-					Vector2 push = (dir + new Vector2 (0, 0.3f)) * knockback;
-					hit.collider.GetComponent<Rigidbody2D> ().AddForce (push, ForceMode2D.Impulse);
-				}
-			}
+			StartCoroutine (Attack ());
 		}
+	}
+
+	void Stun () {
+		anim.SetTrigger ("Hurt");
 	}
 }
