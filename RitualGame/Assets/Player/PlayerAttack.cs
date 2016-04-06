7623e48d50b87160073cee20b260006aca5285d2
Greg Weil <gregweil@pentavera.com>
2016-04-06 17:06:11 -0400

    Modified player attacks, cap punch rate

--- RitualGame/Assets/Player/PlayerAttack.cs
+++ RitualGame/Assets/Player/PlayerAttack.cs
@@ -6,6 +6,7 @@ public class PlayerAttack : MonoBehaviour {
 	float attackRange = 0.8f;
 
 	float knockback = 20.0f;
+    float cooldown = 0.0f;
     
 	Rigidbody2D body = null;
 	PlayerMovement move = null;
@@ -24,9 +25,11 @@ public class PlayerAttack : MonoBehaviour {
 
 	IEnumerator Attack () {
 		anim.SetTrigger ("Attack");
-		yield return new WaitForSeconds (0.1f);
+        cooldown = 0.2f;
+		yield return new WaitForSeconds (0.15f);
+        Vector2 pos = body.position + (0.8f * Vector2.up);
 		Vector2 dir = GetComponentInChildren<SpriteRenderer> ().flipX ? Vector2.left : Vector2.right;
-		var hits = Physics2D.CircleCastAll (body.position, attackRadius, dir, attackRange, LayerMask.GetMask ("Player"));
+		var hits = Physics2D.CircleCastAll (pos, attackRadius, dir, attackRange, LayerMask.GetMask ("Player"));
 		foreach (var hit in hits) {
 			if (hit.collider.gameObject != gameObject) {
 				hit.collider.SendMessage ("Stun", 5.0f, SendMessageOptions.DontRequireReceiver);
@@ -40,7 +43,8 @@ public class PlayerAttack : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (GameMaster.gm.paused) { return; }
-		if (Input.GetButtonDown ("Attack" + controlNum.ToString ()) && !move.HasIdol()) {
+        cooldown -= Time.deltaTime;
+		if (Input.GetButtonDown ("Attack" + controlNum.ToString ()) && !move.HasIdol() && (cooldown < 0f)) {
 			StartCoroutine (Attack ());
 		}
 	}
